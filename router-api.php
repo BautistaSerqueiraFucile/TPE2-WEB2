0bc86662e86b4c16dad28f4d66615b6ab367abbf
mibauza <matiasibauza@gmail.com>
2023-06-24 17:37:43 -0300

    Se da inicio al TPE2

---
+++ router-api.php
@@ -0,0 +1,19 @@
+<?php
+define('URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']) . '/');
+require_once "route.php";
+require_once "mvc/api.controller.php";
+
+$route = new Router();
+
+$route->addRoute("pc","GET","apiController","getPc");
+$route->addRoute("pc/:ID","GET","apiController","getPc");
+$route->addRoute("pc/:ID","DELETE","apiController","deletePc");
+$route->addRoute("pc/","PUT","apiController","createPc");
+$route->addRoute("pc/:ID","POST","apiController","modifiePc");
+
+$url = $_GET['recurso'];
+$verbo= $_SERVER['REQUEST_METHOD'];
+$route->route($url, $verbo);
+
+
+
