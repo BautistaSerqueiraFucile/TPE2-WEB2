60691916796d2db1353bb167fe730b62f64c687f
Bautista Serqueira Fucile <id640794@outlook.com>
2023-07-03 22:06:10 -0300

    Actualizacion de base de datos final

--- Levantar BBDD/db_pcshop.sql
+++ Levantar BBDD/db_pcshop.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-07-2023 a las 01:33:17
--- Versión del servidor: 10.4.25-MariaDB
--- Versión de PHP: 8.1.10
+-- Tiempo de generación: 04-07-2023 a las 03:04:55
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,7 +31,7 @@ CREATE TABLE `gama` (
   `id_gama` int(11) NOT NULL,
   `name_gama` varchar(40) NOT NULL,
   `description_gama` varchar(400) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `gama`
@@ -41,8 +41,7 @@ INSERT INTO `gama` (`id_gama`, `name_gama`, `description_gama`) VALUES
 (28, 'Gama baja', 'La gama baja o de entrada, como se suele nombrar, es donde se encuentran las computadoras con los requisitos mínimos modernos para considerarse gamers. Estas suelen incluir procesadores recientes como la serie 3000 de AMD o los i3 de novena generación de Intel, así como 8 gigabytes de memoria RAM.'),
 (29, 'Gama media', 'La gama media es donde se ubican la gran mayoría de computadoras y componentes, debido a que su espectro de capacidades es muy amplio.'),
 (32, 'Gama alta', 'Las computadoras de alta gama se destacan por tener procesadores más rápidos, pantallas con resolución alta y mayor rendimiento.'),
-(39, 'Gamer', 'gama para pc de jugadores'),
-(40, '4124321', '12414');
+(39, 'Gamer', 'gama para pc de jugadores');
 
 -- --------------------------------------------------------
 
@@ -59,26 +58,15 @@ CREATE TABLE `pc` (
   `description_pc` varchar(400) NOT NULL,
   `RAM` varchar(50) NOT NULL,
   `id_gama` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `pc`
 --
 
 INSERT INTO `pc` (`id_pc`, `motherboard`, `processor`, `disco`, `video`, `description_pc`, `RAM`, `id_gama`) VALUES
-(46, 'pc lili', '123123', '12313', '3123', '111', '13123', 29),
-(48, '2', '222222222222222', '333333333333', '4444444444444', 'asdasdasd', 'asdasda', 28),
-(49, '4', '111111111111', '666666666666', '4444444444444', 'asdasdasd', 'asdasda', 28),
-(57, '3', '111', 'AFGADFG', 'pc modificada', 'pc modificada', 'pc', 28),
-(58, '5', '111', 'AFGADFG', 'pc modificada', 'pc modificada', 'pc', 28),
-(59, '1', '111', '11', '11', '111', '1111', 39),
-(60, '10', 'token', 'token', 'token', '111', '1111', 39),
-(61, '1', '1', '1', '1', '1', '1', 39),
-(62, 'ñkjdfn´gsjdn', '´ñiajsfbdpiag', 'ttt1', 'ttt1', '1', '1', 39),
-(63, '555555', '5555', '5555', '5555', '55555', '5555', 28),
-(64, 'pc nueva', '´pc nueva', 'pc nueva', 'ttt1', '1', '1', 39),
-(65, 'modificada', '´modificada', 'pc nueva', 'ttt1', '1', '1', 39),
-(66, 'cambios 3', 'cambios 3', 'cambios 3', 'cambios 3', 'cambios', 'cambios', 28);
+(51, 'AB-350M', 'Core i5-12400F', 'SSD M.2 1TB', 'NVIDIA® Geforce RTX 3060Ti 8GB GDDR6', 'La PC Banghó Game Master 220T, fue creada para aportar mayor velocidad en el juego y portabilidad. Equipada con Procesador Intel Core i5 de 12va generación y placa gráfica NVIDIA Geforce RTX 3060Ti de 8GB.', '16GB', 39),
+(52, 'AB-310M', 'Core i5-16400F', 'SSD M.2 2TB', 'NVIDIA® Geforce RTX 3090Ti 8GB GDDR6', 'La PC Banghó Game Master 220T, fue creada para aportar mayor velocidad en el juego y portabilidad. Equipada con Procesador Intel Core i5 de 12va generación y placa gráfica NVIDIA Geforce RTX 3060Ti de 8GB.', '8GB', 39);
 
 -- --------------------------------------------------------
 
@@ -92,14 +80,16 @@ CREATE TABLE `user` (
   `user_password` varchar(500) NOT NULL,
   `token` text NOT NULL,
   `time` int(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `user`
 --
 
 INSERT INTO `user` (`id_user`, `username`, `user_password`, `token`, `time`) VALUES
-(26, 'mati', '$2y$10$mfI9ju3bHyQZq1lh5OUm2OuLpUabs133Z0OUIFEUb1IMQ8ZCB5euy', 'e197e42ac1871d4be9e85d159fbf7e8681f40b3d0e97ca933949735112682258', 1688425442);
+(26, 'mati', '$2y$10$mfI9ju3bHyQZq1lh5OUm2OuLpUabs133Z0OUIFEUb1IMQ8ZCB5euy', 'cff0205e1d9e83291727239b694777f720b48e908ad947f863ee4969cafe4e59', 1688430428),
+(28, 'Profe', '$2y$10$5Fknr8sYHjU.x4aL/w5r2eTYWyctzRlLQzQry.jGlt8uHytMVSr.6', '', 0),
+(29, 'Bauti', '$2y$10$RiknvtrNKF5m8Y3S/jOlUegsuyn4jPSDoCM0Z9yeHLAKGh9Ywbs1G', '', 0);
 
 --
 -- Índices para tablas volcadas
@@ -138,13 +128,13 @@ ALTER TABLE `gama`
 -- AUTO_INCREMENT de la tabla `pc`
 --
 ALTER TABLE `pc`
-  MODIFY `id_pc` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
+  MODIFY `id_pc` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
 
 --
 -- AUTO_INCREMENT de la tabla `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- Restricciones para tablas volcadas
